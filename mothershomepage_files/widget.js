2661cbd8613e94d7b60b34769e52ad8f3b8da91f
nickpellegrini <nick@localupsolutions.com>
2012-03-22 15:09:47 -0400

    make Affbar work with Jasa

---
+++ mothershomepage_files/widget.js
@@ -0,0 +1,3 @@
+function displayWidget(myid) {
+    var winWidget=dhtmlwindow.open("widgetBox", "iframe", urlHome + "/panel.php?_node=widgetSignup&user_id=" + myid + "&step=1", "Join our Mobile Rewards Club", "width=590px,height=570px,resize=1,scrolling=1,center=1", "recal");
+}
